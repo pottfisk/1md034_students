5f4a6685e59b4791e502946f47bdf24256a11071
pottfisk <robert.paananen@hotmail.com>
2020-02-03 18:57:06 +0100

    Added js to files

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -18,3 +18,8 @@ console.log(b3.info());
 console.log(b4.info());
 console.log(b5.info());
 
+document.getElementById("burg1").innerHTML = b1.info();
+document.getElementById("burg2").innerHTML = b2.info();
+document.getElementById("burg3").innerHTML = b3.info();
+document.getElementById("burg4").innerHTML = b4.info();
+document.getElementById("burg5").innerHTML = b5.info();
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,10 @@
 const vm = new Vue({
-  el: '#myID',
+  el: '#test',
   data: {
-    arbitraryVariableName: 'Välj en burgare'
+      burg1: b1.info(),
+      burg2: b2.info(),
+      burg3: b3.info(),
+      burg4: b4.info(),
+      burg5: b5.info()
   }
 })
