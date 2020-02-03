9f0440337cfc727177ff6b2e43098cd6babb9f14
pottfisk <robert.paananen@hotmail.com>
2020-02-03 21:17:43 +0100

    Pure js

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -6,20 +6,48 @@ function menuItem(name, desc, kcal, allergies, imgPath){
     this.imgPath = imgPath;
     this.info = () => this.name + ": " + this.kcal + " kcal";
     this.allInfo = function(){
-   	let str = "\n";
-    for(let i = 0; i < this.allergies.length; i++){
-    	str+=this.allergies[i] + "\n";
-    }
-    return str;
+   	let str = "";
+	for(let x in this.allergies){
+    	    str+="<li>"+allergies[x]+"<\li>";
+	}
+	return str;
     }
 }
 
-let b1 = new menuItem("The purger", "Tasty", 2002, ["Lactose", "Gluten"]);
-let b2 = new menuItem("The sturger", "Tasty", 2030, ["Lactose", "Gluten"]);
-let b3 = new menuItem("The mürger", "Tasty", 2004, ["Lactose", "Gluten"]);
-let b4 = new menuItem("The lurger", "Tasty", 2005, ["Lactose", "peanuts"]);
-let b5 = new menuItem("The urger", "Tasty", 2006, ["Lactose", "Gluten"]);
-console.log(b1.info());
+let b1 = new menuItem("The purger", "Pulled pork with hot sauce", 2002, ["Lactose", "Gluten"], "https://cdn.whatsgabycooking.com/wp-content/uploads/2016/12/Pork-Board-Pulled-Pork-5-copy.jpg");
+let b2 = new menuItem("The sturger", "Sturgeon burger with fishy sauce", 2030, ["Lactose", "Gluten"], "https://media-cdn.tripadvisor.com/media/photo-s/10/3b/40/7b/sturgeon-burger.jpg");
+let b3 = new menuItem("The mürger", "Müsli burger with yoghurt", 2004, ["Lactose", "Gluten"], "https://irepo.primecp.com/2016/03/270700/recipe-27807_ArticleImage-CategoryPage_ID-1541467.jpg?v=1541467");
+let b4 = new menuItem("The lurger", "Tasty", 2005, ["Lactose", "peanuts"],"https://irepo.primecp.com/2016/03/270700/recipe-27807_ArticleImage-CategoryPage_ID-1541467.jpg?v=1541467");
+let b5 = new menuItem("The urger", "Tasty", 2006, ["Lactose", "Gluten"],"https://irepo.primecp.com/2016/03/270700/recipe-27807_ArticleImage-CategoryPage_ID-1541467.jpg?v=1541467");
+let menu = [b1,b2,b3,b4,b5];
+
+let wrapper = document.getElementById("wrapper")
+let str = "" + 100/menu.length + "% "
+wrapper.style.gridTemplateColumns = str.repeat(menu.length); 
+
+for(let i = 0; i < menu.length; i++){
+    let burgerItem = document.createElement("div");
+    let burgerTitle = document.createElement("h3");
+    burgerTitle.innerHTML = menu[i].name;
+    let burgerImg  = document.createElement("img");
+    burgerImg.setAttribute('src', menu[i].imgPath);
+    let burgerValue = document.createElement("h4");
+    burgerValue.innerHTML = menu[i].desc;
+    let burgerAllergy = document.createElement("ul");
+    for(let x in menu[i].allergies){
+	let listItem = document.createElement("li");
+	listItem.innerHTML = menu[i].allergies[x];
+	burgerAllergy.appendChild(listItem);
+    }
+    burgerItem.appendChild(burgerTitle);
+    burgerItem.appendChild(burgerImg);
+    burgerItem.appendChild(burgerValue);
+    burgerItem.appendChild(burgerAllergy);
+    wrapper.appendChild(burgerItem);
+}
+
+
+/*console.log(b1.info());
 console.log(b2.info());
 console.log(b3.info());
 console.log(b4.info());
@@ -32,7 +60,7 @@ document.getElementById("burg4").innerHTML = b4.info();
 document.getElementById("burg5").innerHTML = b5.info();
 
 
-let menu = [b1,b2,b3,b4,b5];
+
 let myElement = document.getElementById("test");
 for(let i = 0; i < menu.length; i++){
   let listItem = document.createElement("p");
@@ -40,3 +68,4 @@ for(let i = 0; i < menu.length; i++){
   listItem.appendChild(listValue);
   myElement.appendChild(listItem);
 }
+*/
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,12 +1,13 @@
-const vm = new Vue({
+/*const vm = new Vue({
   el: '#test',
   data: {
   	menuCopy: menu
-  	  /*
+  	  
   	burg1: b1.info(),
     burg2: b2.info(),
     burg3: b3.info(),
     burg4: b4.info(),
     burg5: b5.info()
- */ }
+  }
 })
+*/
