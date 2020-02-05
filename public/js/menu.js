8aa3f7c0a7a34ce24aaac87adf649cfab7da8eb3
pottfisk <robert.paananen@hotmail.com>
2020-02-05 12:01:26 +0100

    Moved checkbox and added menu.js

---
+++ public/js/menu.js
@@ -0,0 +1,42 @@
+let food = [
+    {
+	"name": "The Purger",
+	"desc": "Tasty",
+	"kcal": 850,
+	"lactose": true,
+	"gluten": false,
+	"imgPath": "https://cdn.whatsgabycooking.com/wp-content/uploads/2016/12/Pork-Board-Pulled-Pork-5-copy.jpg"
+    },
+    {
+	"name": "The Sturger",
+	"desc": "Tasty",
+	"kcal": 850,
+	"lactose": true,
+	"gluten": false,
+	"imgPath": "https://media-cdn.tripadvisor.com/media/photo-s/10/3b/40/7b/sturgeon-burger.jpg"
+    },
+    {
+	"name": "The Mürger",
+	"desc": "Tasty",
+	"kcal": 850,
+	"lactose": true,
+	"gluten": false,
+	"imgPath": "https://irepo.primecp.com/2016/03/270700/recipe-27807_ArticleImage-CategoryPage_ID-1541467.jpg?v=1541467"
+    },
+    {
+	"name": "The Lurger",
+	"desc": "Tasty",
+	"kcal": 850,
+	"lactose": true,
+	"gluten": true,
+	"imgPath": "https://cdn.whatsgabycooking.com/wp-content/uploads/2016/12/Pork-Board-Pulled-Pork-5-copy.jpg"
+    },
+    {
+	"name": "The Urger",
+	"desc": "Tasty",
+	"kcal": 850,
+	"lactose": true,
+	"gluten": true,
+	"imgPath": "https://cdn.whatsgabycooking.com/wp-content/uploads/2016/12/Pork-Board-Pulled-Pork-5-copy.jpg"
+    }  
+]
